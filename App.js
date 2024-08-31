9ddcaba3f67f1ea40533c131a9248449546acdd2
Chiyembekezo <phoenixtech2003@gmail.com>
2024-08-31 21:06:04 +0200

    Add the font Awesome library for the button

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/imageViewer';
-
+import Button from './components/button';
 const placeHolderImage = require("./assets/images/background-image.png")
 
 export default function App() {
@@ -10,6 +10,10 @@ export default function App() {
       <View style={styles.imageContainer}>
 
       <ImageViewer placeHolderImageSource={placeHolderImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label={"Choose a Photo"}/>
+        <Button label={"Use this Photo"}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -31,5 +35,9 @@ const styles = StyleSheet.create({
     width: 320,
     height:440,
     borderRadius:18,
+  },
+  footerContainer:{
+    flex:1/3,
+    alignItems: 'center'
   }
 });
