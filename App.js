7a11c47bf0168c4ec67e0d2615bd319c00047645
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-10 21:12:48 +0200

    Add the conditional render for clicking the buttons

--- App.js
+++ App.js
@@ -1,35 +1,51 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import ImageViewer from './components/imageViewer';
-import Button from './components/button';
-const placeHolderImage = require("./assets/images/background-image.png")
-import * as ImagePicker from 'expo-image-picker'
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import ImageViewer from "./components/imageViewer";
+import Button from "./components/button";
+const placeHolderImage = require("./assets/images/background-image.png");
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = useState(null)
-  const pickImageAsync = async()=>{
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
-      quality:1,
+      quality: 1,
     });
 
-    if(!result.canceled){
+    if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
-    }else{
-      alert("you did not select any image")
+      setShowAppOptions(true);
+    } else {
+      alert("you did not select any image");
     }
-  }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
+        <ImageViewer
+          placeHolderImageSource={placeHolderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label={"Choose a Photo"}
+            onPress={pickImageAsync}
+          />
+          <Button
+            label={"Use this Photo"}
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
 
-      <ImageViewer placeHolderImageSource={placeHolderImage} selectedImage={selectedImage}/>
-      </View>
-      <View style={styles.footerContainer}>
-        <Button theme='primary' label={"Choose a Photo"} onPress={pickImageAsync}/>
-        <Button label={"Use this Photo"}/>
-      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -38,21 +54,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  imageContainer:{
-    flex:1,
-    paddingTop:58,
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
-  image:{
+  image: {
     width: 320,
-    height:440,
-    borderRadius:18,
+    height: 440,
+    borderRadius: 18,
   },
-  footerContainer:{
-    flex:1/3,
-    alignItems: 'center'
-  }
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
 });
